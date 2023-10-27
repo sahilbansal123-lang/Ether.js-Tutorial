4fcfa7c8745e8d4cf566ea55b306f593ee9a168e
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-27 10:14:47 +0530

    test

--- InteractionSC.js
+++ InteractionSC.js
@@ -1,8 +1,8 @@
 const { ethers } = require("ethers");
-const provider = new ethers.provider.JsonRpcProvider(
-  `https://goerli.infura.io/v3/2704052c48154c0ea15c57b4d3e05127`
+const provider = new ethers.providers.JsonRpcProvider(
+    `https://sepolia.infura.io/v3/2704052c48154c0ea15c57b4d3e05127`
 );
-const walletAddress = "0x0E385fabb7C8298957c96afEAdFC6C640Ada58A6";
+const walletAddress = "0x534cc583ea96cbc624b2235fa58cf63c22c60e97";
 const walletAbi = [
   {
     inputs: [
@@ -97,10 +97,28 @@ const walletAbi = [
   },
 ];
 
-const contractnteraction = (async = () => {
+const contractnteraction = async () => {
   const walletContract = new ethers.Contract(
     walletAddress,
     walletAbi,
     provider
   );
-});
+
+  const contracName = await walletContract.name();
+  console.log("Contract Name:", contracName);
+
+
+  const num = await walletContract.getValue();
+  console.log("Number Value:", String(num));
+
+  const contractBalance = await walletContract.contractBalance();
+  const balanceEthContract = await ethers.utils.formatEther(contractBalance);
+  console.log("contract Balance:", balanceEthContract);
+
+  const userBalance = await walletContract.accountBalance("0x0E385fabb7C8298957c96afEAdFC6C640Ada58A6")
+  const balanceEthUser = await ethers.utils.formatEther(userBalance);
+  console.log("User Balance: ", balanceEthUser);
+
+};
+
+contractnteraction();
